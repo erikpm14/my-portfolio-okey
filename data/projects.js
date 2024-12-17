84a52a93590f2fb6e08756cb80d0df3ec7ff0b86
erikpm14 <portilloerik14@gmail.com>
2024-12-17 22:25:12 +0100

    new

---
+++ data/projects.js
@@ -0,0 +1,71 @@
+// export const projects = [
+
+//     {tittle: "Clima App",
+//       img: "https://cdn.computerhoy.com/sites/navi.axelspringer.es/public/media/image/2021/07/app-tiempo-iphone-2406843.jpg?tf=1200x1200",
+//       description: "Una aplicación web que proporciona información meteorológica en tiempo real. Los usuarios pueden buscar cualquier ciudad del mundo y obtener detalles como la temperatura, la humedad y el pronóstico para los próximos días.",
+//       technologies: "HTML, CSS, JavaScript, API de OpenWeatherMap",
+//       link: "https://example.com/clima-app"
+//     },
+  
+//     {tittle: "Biblioteca Virtual",
+//       img: "https://bibliotecas.larioja.org/images/blog_img/a_biblioteca_virtual.jpg",
+//       description: "Una aplicación web que permite a los usuarios gestionar su colección de libros. Los usuarios pueden agregar, editar y eliminar libros, así como marcar sus libros favoritos. Incluye una opción para reseñas y valoraciones de los libros.",
+//       technologies: "React, Node.js, Express, MongoDB",
+//       link: "https://example.com/biblioteca-virtual"
+//     },
+  
+//     {tittle: "Calculadora",
+//       img: "https://help.apple.com/assets/66EB2D93D8C3BD635E04A0A9/66EB2D97D8C3BD635E04A0AF/es_ES/5250122ee560cee90b3d7cfa2d91695e.png",
+//       description: "Una aplicación intuitiva que permite realizar operaciones matemáticas básicas como suma, resta, multiplicación y división. Diseñada con una interfaz amigable y responsive.",
+//       technologies: "JavaScript, HTML, CSS",
+//       link: "https://example.com/calculator"
+//     }
+//    ]
+  
+export const projects = [
+    {
+      title: "Primer proyecto",
+      description: "Esta es la descripción de mi primer proyecto",
+      image:
+        "https://images.unsplash.com/photo-1498050108023-c5249f4df085?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8d2VifGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60",
+      tech: ["React", "HTML5", "CSS3", "JavaScript"],
+      github: "http://github.com/myuser/proyecto1",
+      link: "www.proyecto1.com",
+    },
+    {
+      title: "Segundo proyecto",
+      description: "Esta es la descripción de mi segundo proyecto",
+      image:
+        "https://images.unsplash.com/photo-1661961111184-11317b40adb2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2344&q=80",
+      tech: ["React", "HTML5", "CSS3", "JavaScript"],
+      github: "http://github.com/myuser/proyecto2",
+      link: "www.proyecto1.com",
+    },
+    {
+      title: "Tercer proyecto",
+      description: "Esta es la descripción de mi tercer proyecto",
+      image:
+        "https://images.unsplash.com/photo-1669570094762-828f3dfaf675?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3270&q=80",
+      tech: ["React", "HTML5", "CSS3", "JavaScript"],
+      github: "http://github.com/myuser/proyecto3",
+      link: "www.proyecto1.com",
+    },
+    {
+      title: "Cuarto proyecto",
+      description: "Esta es la descripción de mi cuarto proyecto",
+      image:
+        "https://images.unsplash.com/photo-1664575198308-3959904fa430?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2340&q=80",
+      tech: ["React", "HTML5", "CSS3", "JavaScript"],
+      github: "http://github.com/myuser/proyecto4",
+      link: "www.proyecto1.com",
+    },
+    {
+      title: "Quinto proyecto",
+      description: "Esta es la descripción de mi quinto proyecto",
+      image:
+        "https://images.unsplash.com/photo-1665686306574-1ace09918530?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3387&q=80",
+      tech: ["React", "HTML5", "CSS3", "JavaScript"],
+      github: "http://github.com/myuser/proyecto5",
+      link: "www.proyecto1.com",
+    },
+  ];
---
+++ utils/cleanPage.js
@@ -0,0 +1,3 @@
+export function cleanPage(container) {
+    container.innerHTML = "";
+  };
