19f8e7cf79c6016115c4fd8e48775cdaec02a7fb
erikpm14 <portilloerik14@gmail.com>
2024-12-20 15:35:31 +0100

    f

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 import "./style.css";
-import { changeTheme } from "/components/Navbar/Navbar";
-import { linkPage } from "/utils/linkPage";
-import { Navbar } from "/components/Navbar/Navbar";
+import { changeTheme } from "/components/Navbar/Navbar.js";
+//import { linkPage } from "/utils/linkPage.js";
+import { Navbar } from "/components/Navbar/Navbar.js";
 import { Footer } from "./components/Footer/Footer.js";
-import { Home } from "/pages/Home/Home";
-import { Projects } from "/pages/Projects/Projects";
+import { Home } from "/pages/Home/Home.js";
+//import { Projects } from "/pages/Projects/Projects.js";
 import { Divider } from "./components/Divider/Divider.js";
 
 const header = document.querySelector("header");
@@ -12,8 +12,8 @@ header.innerHTML = Navbar();
 const footer = document.querySelector("footer");
 footer.innerHTML = Footer();
 
-linkPage("#homelink", Home);
-linkPage("#projectslink", Projects);
+//linkPage("#homelink", Home);
+//linkPage("#projectslink", Projects);
 
 Home();
 
